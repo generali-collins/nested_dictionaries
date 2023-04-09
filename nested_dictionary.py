8ae39560e3ed7333770e7ff426ba732baa34dd6b
generali.collins <104697851+generali-collins@users.noreply.github.com>
2023-04-09 22:33:01 +0000

    Add files via upload

---
+++ nested_dictionary.py
@@ -0,0 +1,11 @@
+# Collins Wanga
+
+
+# Creating a Dictionary
+Dict = {'Dict1': {1: 'Manchester'},
+		'Dict2': {'Coach': 'Eric'}}
+
+# Accessing element using key
+print(Dict['Dict1'])
+print(Dict['Dict1'][1])
+print(Dict['Dict2']['Coach'])
